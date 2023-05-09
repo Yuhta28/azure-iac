964fd85faf0fa29341a83ec87ad9b19d5a654a33
Yuta <yuta_mori@outlook.jp>
2023-05-10 00:48:38 +0900

    add module

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,19 +13,12 @@ terraform {
   }
 }
 
-module "caf" {
-  source          = "aztfmod/caf/azurerm"
-  version         = "5.7.0-preview0"
-  global_settings = var.global_settings
-  resource_groups = var.resource_groups
-  keyvaults       = var.keyvaults
+module "vnet" {
+  source  = "Azure/vnet/azurerm"
+  version = "4.0.0"
+  # insert the 3 required variables here
+  resource_group_name = "Test"
+  use_for_each = "false"
+  vnet_location = var.azure_location
 
-  compute = {
-    virtual_machines = var.virtual_machines
-  }
-
-  networking = {
-    public_ip_addresses = var.public_ip_addresses
-    vnets               = var.vnets
-  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,22 +4,3 @@ variable "azure_location" {
   description = "Azure location for all resources"
 }
 
-variable "virtual_machines" {
-  default     = {}
-  description = "Create a virtual machine from CAF module"
-}
-variable "resource_groups" {
-  default = {}
-}
-variable "vnets" {
-  default = {}
-}
-variable "public_ip_addresses" {
-  default = {}
-}
-variable "keyvaults" {
-  default = {}
-}
-variable "global_settings" {
-  default = {}
-}
