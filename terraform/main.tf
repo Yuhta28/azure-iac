521a3faadb655ff8d4f59140b24b97335e089bad
Yuta <yuta_mori@outlook.jp>
2023-05-10 00:43:33 +0900

    add module

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   cloud {
-     organization = "Yuta28"
+    organization = "Yuta28"
     workspaces {
       name = "azure-iac"
     }
@@ -14,6 +14,18 @@ terraform {
 }
 
 module "caf" {
-  source  = "aztfmod/caf/azurerm"
-  version = "5.7.0-preview0"
+  source          = "aztfmod/caf/azurerm"
+  version         = "5.7.0-preview0"
+  global_settings = var.global_settings
+  resource_groups = var.resource_groups
+  keyvaults       = var.keyvaults
+
+  compute = {
+    virtual_machines = var.virtual_machines
+  }
+
+  networking = {
+    public_ip_addresses = var.public_ip_addresses
+    vnets               = var.vnets
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,23 @@ variable "azure_location" {
   default     = "eastus"
   description = "Azure location for all resources"
 }
+
+variable "virtual_machines" {
+  default     = {}
+  description = "Create a virtual machine from CAF module"
+}
+variable "resource_groups" {
+  default = {}
+}
+variable "vnets" {
+  default = {}
+}
+variable "public_ip_addresses" {
+  default = {}
+}
+variable "keyvaults" {
+  default = {}
+}
+variable "global_settings" {
+  default = {}
+}
