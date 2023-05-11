6f452cd586726720f0d3413730cef35c02a2866d
Yuta <yuta_mori@outlook.jp>
2023-05-11 22:54:10 +0900

    add app service

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -22,3 +22,22 @@ resource "azurerm_resource_group" "demo" {
   name     = azurecaf_name.rg_example.result
   location = var.azure_location
 }
+
+resource "azurerm_service_plan" "demo_plan" {
+  name = azurecaf_name.rg_example.result
+  resource_group_name = azurerm_resource_group.demo.name
+  location = var.azure_location
+  os_type = "Linux"
+  sku_name = "F1"
+}
+
+resource "azurerm_linux_web_app" "demo_app" {
+  name = azurecaf_name.rg_example.result
+  location = var.azure_location
+  resource_group_name = azurerm_resource_group.demo.name
+  service_plan_id = azurerm_service_plan.demo_plan.id
+
+  site_config {
+    
+  }
+}
