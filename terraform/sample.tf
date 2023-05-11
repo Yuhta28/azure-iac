abaf15afa0b5028be282aafc2e9d7ca20fdb4c76
Yuta <yuta_mori@outlook.jp>
2023-05-12 00:58:19 +0900

    fix azurecaf name

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -11,34 +11,34 @@ provider "azurecaf" {
 }
 
 resource "azurecaf_name" "naming_define" {
-  name          = "demogroup"
-  resource_types = ["azurerm_resource_group","azurerm_app_service_plan", "azurerm_app_service"]
-  prefixes      = ["ctf"]
-  suffixes      = ["terraform"]
-  random_length = 3
-  clean_input   = true
+  name           = "demogroup"
+  resource_types = ["azurerm_resource_group", "azurerm_app_service_plan", "azurerm_app_service"]
+  prefixes       = ["ctf"]
+  suffixes       = ["terraform"]
+  random_length  = 3
+  clean_input    = true
 }
 
 resource "azurerm_resource_group" "demo" {
-  name     = azurecaf_name.naming_define.results[1]
+  name     = values(azurecaf_name.naming_define.results)[0]
   location = var.azure_location
 }
 
 resource "azurerm_service_plan" "demo_plan" {
-  name = azurecaf_name.naming_define.results[2]
+  name                = values(azurecaf_name.naming_define.results)[1]
   resource_group_name = azurerm_resource_group.demo.name
-  location = var.azure_location
-  os_type = "Linux"
-  sku_name = "F1"
+  location            = var.azure_location
+  os_type             = "Linux"
+  sku_name            = "F1"
 }
 
 resource "azurerm_linux_web_app" "demo_app" {
-  name = azurecaf_name.naming_define.results[3]
-  location = var.azure_location
+  name                = values(azurecaf_name.naming_define.results)[2]
+  location            = var.azure_location
   resource_group_name = azurerm_resource_group.demo.name
-  service_plan_id = azurerm_service_plan.demo_plan.id
+  service_plan_id     = azurerm_service_plan.demo_plan.id
 
   site_config {
-    
+
   }
 }
