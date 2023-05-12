bdacb7a0e00950fa34084eaf4bbddcb063309bb7
Yuta <yuta_mori@outlook.jp>
2023-05-12 22:56:53 +0900

    add module vnet

--- terraform/vnet.tf
+++ terraform/vnet.tf
@@ -7,9 +7,9 @@ module "vnet" {
   use_for_each        = false
   vnet_location       = module.regions.location
 
-  address_space = ["10.1.0.0/16"]
+  address_space   = ["10.1.0.0/16"]
   subnet_prefixes = ["10.1.0.0/24", "10.1.2.0/24", "10.1.3.0/24"]
-  subnet_names = [values(azurecaf_name.vnet_naming.results)[2], values(azurecaf_name.vnet_naming.results)[2], values(azurecaf_name.vnet_naming.results)[2]]
+  subnet_names    = [values(azurecaf_name.vnet_naming.results)[2], values(azurecaf_name.vnet_naming.results)[2], values(azurecaf_name.vnet_naming.results)[2]]
   vnet_name       = values(azurecaf_name.vnet_naming.results)[1]
 }
 
@@ -17,7 +17,7 @@ resource "azurecaf_name" "vnet_naming" {
   name           = "vnet_nameing"
   resource_types = ["azurerm_resource_group", "azurerm_virtual_network", "azurerm_subnet"]
   prefixes       = ["ctf"]
-  suffixes       = ["vnet","001","002"]
+  suffixes       = ["vnet", "001", "002"]
   random_length  = 3
   clean_input    = true
 }
@@ -25,4 +25,7 @@ resource "azurecaf_name" "vnet_naming" {
 resource "azurerm_resource_group" "vnet_rg" {
   name     = values(azurecaf_name.vnet_naming.results)[0]
   location = module.regions.location
+  tags = {
+    "IaC" : "Terraform"
+  }
 }
